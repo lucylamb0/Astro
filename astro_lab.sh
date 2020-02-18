86f5f8d282f4ac689da664687ab282db525d4b00
Maxime Delorme <Maxime.Delorme@gmail.com>
2020-02-18 12:46:30 +0000

    Bugfix python upgrade

--- astro_lab.sh
+++ astro_lab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PYTHONPATH=/user/HS103/md0046/.local/lib/python3.5/site-packages/:/user/HS103/md0046/tools/photutils/install/lib/python3.5/site-packages/:/user/HS103/md0046/tools/astropy/install/lib/python3.5/site-packages/:/user/HS103/md0046/tools/lmfit-py/install/lib/python3.5/site-packages/:$PYTHONPATH
+export PYTHONPATH=/user/HS103/md0046/.local/lib/python3.6/site-packages/:$PYTHONPATH
 
 ipython3 -i astro_lab.py
